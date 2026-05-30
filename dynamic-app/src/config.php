5a3eab15fab921bb7dc435029d8baf7a11328ac1
ureGamine <cpg170206@gmail.com>
2026-05-30 15:28:29 +0700

    Update dynamic app for zero-touch test

--- dynamic-app/src/config.php
+++ dynamic-app/src/config.php
@@ -5,7 +5,7 @@
 // TIDAK ada hardcode credential di sini.
 // ============================================================
 
-define('APP_NAME',    'UAS Cloud Computing II');
+define('APP_NAME',    'UAS Administrasi Server');
 define('APP_VERSION', '1.0.0');
 define('APP_NIM',     '2388010022');
 
